2684bece4858f15435564cf2d0f6456c32c09559
Ivan Babanin <ivan@babanin.net>
2023-07-22 23:25:37 +0500

    Refactor Record boostrap method generator

--- src/main/kotlin/katartal/model/method/LocalsBuilder.kt
+++ src/main/kotlin/katartal/model/method/LocalsBuilder.kt
@@ -1,8 +1,6 @@
 package katartal.model.method
 
-import katartal.model.CPoolIndex
 import katartal.model.ConstantPool
-import katartal.util.descriptor
 
 class LocalsBuilder(val constantPool: ConstantPool) {
 
--- src/main/kotlin/katartal/util/Utils.kt
+++ src/main/kotlin/katartal/util/Utils.kt
@@ -5,40 +5,14 @@ fun <T> Class<T>.path(): String {
     return pkg.name.replace(".", "/") + "/" + this.simpleName
 }
 
-private val PRIMITIVES = mapOf<Class<*>, String>(
-    Byte::class.java to "B",
-    Boolean::class.java to "Z",
-    Char::class.java to "C",
-    Double::class.java to "D",
-    Float::class.java to "F",
-    Int::class.java to "I",
-    Long::class.java to "J"
-)
-
 fun <T> Class<T>.descriptor(): String {
-    if (isPrimitive) {
-        return PRIMITIVES[this]!!
-    } else if (isArray) {
-        val sb = StringBuilder()
-
-        var cls: Class<*> = this
-        while (cls.isArray) {
-            sb.append("[")
-            cls = cls.componentType
-        }
-
-        if (cls.isPrimitive) {
-            sb.append(PRIMITIVES[cls])
-        } else {
-            sb.append("L")
-                .append(cls.path())
-                .append(";")
-        }
-
-        return sb.toString()
-    } else if (this == Void::class.java) {
+    if (this == Void::class.java) {
         return "V"
     }
 
-    return "L" + path() + ";"
+    return descriptorString()
+}
+
+fun methodDescriptor(returnType: Class<*>, vararg parameters: Class<*>): String {
+    return parameters.joinToString(separator = "", prefix = "(", postfix = ")") { it.descriptor() } + returnType.descriptor()
 }
