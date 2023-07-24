512c4fc82ba5a4ebb86a0899647414bf8e554194
Ivan Babanin <1204061+babanin@users.noreply.github.com>
2023-07-24 23:47:21 +0500

    Jacoco and Github actions (#14)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,22 @@
 plugins {
     kotlin("jvm") version "1.9.0"
+    jacoco
     `java-library`
 }
 
 group = "net.babanin"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    
+
     maven {
         url = uri("https://repository.ow2.org/nexus/content/repositories/releases/")
     }
 }
 
-dependencies {    
-    testImplementation(kotlin("test"))  
+dependencies {
+    testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
 }
@@ -26,4 +27,23 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+jacoco {
+    toolVersion = "0.8.9"
+
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    
+    reports {
+        xml.required.set(true)
+        csv.required.set(true)
+        html.required.set(true)
+    }
 }
